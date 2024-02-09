63c3ece5f444b1cb9952fa7d27f794316c360a55
Dima Mezhevich <octadim@gmail.com>
2024-02-10 02:55:15 +0300

    +feat: Added custom user model: class User(AbstractBaseUser, PermissionsMixin)

--- apps/api/user/models.py
+++ apps/api/user/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        PermissionsMixin,
+                                        AbstractUser)
+
+from django.contrib.auth.models import UserManager
+
+from django.utils.translation import gettext_lazy
+
+from apps.api.messages_fields import (EMAIL, USERNAME, NICKNAME,
+                                      FIRST_NAME, LAST_NAME, PHONE,
+                                      )
+
+
+class User(AbstractBaseUser, PermissionsMixin):  # todo: Try later to rename CustomUser
+    email = models.EmailField(max_length=100,
+                              unique=True,
+                              verbose_name=gettext_lazy(EMAIL),
+                              )
+
+    username = models.CharField(max_length=30,
+                                unique=True,
+                                verbose_name=gettext_lazy(USERNAME),
+                                )
+
+    nickname = models.CharField(max_length=50,
+                                unique=True,
+                                verbose_name=gettext_lazy(NICKNAME),
+                                )
+
+    first_name = models.CharField(max_length=30,
+                                  verbose_name=gettext_lazy(FIRST_NAME),
+                                  )
+
+    last_name = models.CharField(max_length=30,
+                                 blank=True, null=True,
+                                 verbose_name=gettext_lazy(LAST_NAME),
+                                 )
+
+    phone = models.CharField(max_length=100,
+                             blank=True, null=True,
+                             verbose_name=gettext_lazy(PHONE))
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+
+    data_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(blank=True, null=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email",
+                       "nickname",
+                       "first_name",
+                       # "last_name",
+                       # "phone"
+                       ]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.nickname}"
+
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
