e7d29ebb09f49cef05d55e6486a7b5279f116632
Dima Mezhevich <octadim@gmail.com>
2024-02-10 02:46:36 +0300

    +feat: Changed import from Django standard User to custom User model for the views

--- apps/api/user/views.py
+++ apps/api/user/views.py
@@ -4,20 +4,28 @@ from django.shortcuts import render
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+
 from django.utils.translation import gettext_lazy
+
+# ##################################################################
+# from django.contrib.auth.models import User  # Django default user
+from apps.api.user.models import User  # Custom user
+# ##################################################################
 
 from apps.api.messages import (USERS_NOT_FOUND,
                                ALL_USERS_LIST,
                                )
 
-from apps.api.user.serializers import AllUsersModelSerializer
+from apps.api.user.serializers import AllUsersSerializer
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import (ListAPIView,
+                                     CreateAPIView,
+                                     )
 
-from django.contrib.auth.models import User
+
 
 class ListAllUsersGenericList(ListAPIView):
-    serializer_class = AllUsersModelSerializer
+    serializer_class = AllUsersSerializer
 
     def get_queryset(self):
         users = User.objects.all()
@@ -32,8 +40,12 @@ class ListAllUsersGenericList(ListAPIView):
                                   "data": []}
                             )
 
-        serializer = self.serializer_class(instance=users, many=True)  # ATTENTION: DON'T FORGET many=True
+        serializer = self.serializer_class(instance=users, many=True)  # IMPORTANT: DON'T FORGET many=True
         return Response(status=status.HTTP_200_OK,
                         data={"message": gettext_lazy(ALL_USERS_LIST),
                               "data": serializer.data}
                         )
+
+
+class RegisterNewUserGenericCreate(CreateAPIView):
+    pass
