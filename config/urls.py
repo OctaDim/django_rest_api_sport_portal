9b26dcdc294b846f950e1603f19922ca8b706f6b
Dima Mezhevich <octadim@gmail.com>
2024-02-09 03:48:41 +0300

    +url: Url with empty endpoint "" was linked to the apps.api.router

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("apps.api.router")),  # ATTENTION: DON'T FORGET TO ADD .router to the end
     path("api/v1/", include("apps.api.router")),
 
     # path("front/", include("apps/api/router")),
