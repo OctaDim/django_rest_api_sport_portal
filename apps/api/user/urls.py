1ba554daa5273f33b015e193cbba8f5432de6cca
Dima Mezhevich <octadim@gmail.com>
2024-02-11 01:10:31 +0300

    +url: Added url 'register_new_superuser' with view 'RegisterNewSuperUserGenericCreate'

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from apps.api.user.views import (ListAllUsersGenericList,
                                  RegisterNewUserGenericCreate,
+                                 RegisterNewSuperUserGenericCreate,
                                  )
 
 app_name = "user"
@@ -8,7 +9,7 @@ app_name = "user"
 urlpatterns = [
     path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
     path("register_new_user", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
-    # path("", as_view(), name=""),
+    path("register_new_superuser", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
     # path("", as_view(), name=""),
     # path("", as_view(), name=""),
     # path("", as_view(), name=""),
