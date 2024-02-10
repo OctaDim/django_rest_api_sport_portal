aaf1250c7d99549ee27d7997a21917097bc491e5
Dima Mezhevich <octadim@gmail.com>
2024-02-10 03:08:31 +0300

    +important comment (after fixing error with wrong way in Include())

--- apps/api/router.py
+++ apps/api/router.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 app_name = "router"
 
 urlpatterns = [
-    path('users/', include("apps.api.user.urls")),  # ATTENTION: DON'T FORGET TO ADD .urls to the end
+    path('users/', include("apps.api.user.urls")),  # IMPORTANT: DON'T FORGET TO ADD .urls to the end
 
 ]
