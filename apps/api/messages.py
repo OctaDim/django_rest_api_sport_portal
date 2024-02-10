a3620fe997a2facc8735177a9ca9a57c2f066199
Dima Mezhevich <octadim@gmail.com>
2024-02-10 03:06:06 +0300

    +feat: Defined outer info messages (used in gettext_lazy)

--- apps/api/messages.py
+++ apps/api/messages.py
@@ -1,2 +1,9 @@
-USERS_NOT_FOUND= "Users were not found (No user exists)"
+USERS_NOT_FOUND= "Users were not found. No user exists"
 ALL_USERS_LIST= "All users list:"
+
+ENTER_EMAIL_LIKE="enter email like <any>@<any>"
+ENTER_EMAIL="enter email"
+ENTER_USERNAME="enter username"
+ENTER_NICKNAME="enter nickname"
+ENTER_PASSWORD="input password"
+REPEAT_PASSWORD="repeat password"
