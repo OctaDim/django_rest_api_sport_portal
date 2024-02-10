53a90084a229e1721bdb1ec9b1dade693337a77d
Dima Mezhevich <octadim@gmail.com>
2024-02-10 03:15:46 +0300

    +import: Changed import from Django standard User to custom User model for the serializers

--- apps/api/user/serializers.py
+++ apps/api/user/serializers.py
@@ -1,7 +1,11 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class AllUsersModelSerializer(serializers.ModelSerializer):
+# ##################################################################
+# from django.contrib.auth.models import User  # Django default user
+from apps.api.user.models import User  # Custom user
+# ##################################################################
+
+class AllUsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
