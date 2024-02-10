a4975099a83455ed5d2cebb97b0f0e6545ec26c1
Dima Mezhevich <octadim@gmail.com>
2024-02-10 03:21:47 +0300

    +url: Added url to the api view creating new custom user 'RegisterNewUserGenericCreate'

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from apps.api.user.views import ListAllUsersGenericList
+from apps.api.user.views import (ListAllUsersGenericList,
+                                 RegisterNewUserGenericCreate,
+                                 )
 
 app_name = "user"
 
 urlpatterns = [
-    path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users")
+    path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
+    path("register_new_user", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
+    # path("", as_view(), name=""),
+    # path("", as_view(), name=""),
+    # path("", as_view(), name=""),
+    # path("", as_view(), name=""),
+    # path("", as_view(), name=""),
+
 ]
