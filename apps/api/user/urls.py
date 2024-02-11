9d61a72789fe1de6ae26a6b86b18719d30df7a7c
Dima Mezhevich <octadim@gmail.com>
2024-02-11 17:18:26 +0300

    +url: Added new url 'register_new_staff_user/' with link to the view 'RegisterNewStaffUserGenericCreate' (creating new staff user)

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from apps.api.user.views import (ListAllUsersGenericList,
                                  RegisterNewUserGenericCreate,
                                  RegisterNewSuperUserGenericCreate,
+                                 RegisterNewStaffUserGenericCreate,
                                  )
 
 app_name = "user"
 
 urlpatterns = [
     path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
-    path("register_new_user", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
-    path("register_new_superuser", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
+    path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
+    path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
+    path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
     # path("", as_view(), name=""),
     # path("", as_view(), name=""),
     # path("", as_view(), name=""),
