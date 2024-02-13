b26f55e3e1250afec74b4a176fcc9e0c28078014
Dima Mezhevich <octadim@gmail.com>
2024-02-13 03:07:27 +0300

    +trial code only: Trying my another custom way to create different users (user, superuser, staff)

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -3,6 +3,9 @@ from apps.api.user.views import (ListAllUsersGenericList,
                                  RegisterNewUserGenericCreate,
                                  RegisterNewSuperUserGenericCreate,
                                  RegisterNewStaffUserGenericCreate,
+                                 __TEST_RegisterNewUserGenericCreate,
+                                 __TEST_RegisterNewSuperUserGenericCreate,
+                                 __TEST_RegisterNewStaffUserGenericCreate,
                                  )
 
 app_name = "user"
@@ -12,6 +15,13 @@ urlpatterns = [
     path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
     path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
     path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
+
+    # ################### TRIAL CODE ###################################
+    path("__TEST_register_new_user_v2/", __TEST_RegisterNewUserGenericCreate.as_view(), name="TEST-register-new-user-v2"),
+    path("__TEST_register_new_superuser_v2/", __TEST_RegisterNewSuperUserGenericCreate.as_view(), name="TEST-register-new-superuser-v2"),
+    path("__TEST_register_new_staff_user_v2/", __TEST_RegisterNewStaffUserGenericCreate.as_view(), name="TEST-register-new-staff-user-2"),
+    # ##################################################################
+
     # path("", as_view(), name=""),
     # path("", as_view(), name=""),
     # path("", as_view(), name=""),
