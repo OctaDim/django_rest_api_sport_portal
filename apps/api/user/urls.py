55f91c0945eac0d10c7bf0264f9b69e204f5f60b
Dima Mezhevich <octadim@gmail.com>
2024-02-14 14:13:56 +0300

    +comments: No code changes

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -11,20 +11,20 @@ from apps.api.user.views import (ListAllUsersGenericList,
 app_name = "user"
 
 urlpatterns = [
+    # superuser
     path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
-    path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
-    path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
-    path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
 
-    # ################### TRIAL CODE ###################################
+    # user, staff, superuser
+    path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
+    # staff, superuser
+    path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
+    # superuser
+    path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
+
+    # ################### MY TRIAL CODE ################################
     path("__TEST_register_new_user_v2/", __TEST_RegisterNewUserGenericCreate.as_view(), name="TEST-register-new-user-v2"),
     path("__TEST_register_new_superuser_v2/", __TEST_RegisterNewSuperUserGenericCreate.as_view(), name="TEST-register-new-superuser-v2"),
     path("__TEST_register_new_staff_user_v2/", __TEST_RegisterNewStaffUserGenericCreate.as_view(), name="TEST-register-new-staff-user-2"),
     # ##################################################################
-
-    # path("", as_view(), name=""),
-    # path("", as_view(), name=""),
-    # path("", as_view(), name=""),
-    # path("", as_view(), name=""),
 
 ]
