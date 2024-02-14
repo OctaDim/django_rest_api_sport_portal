6d1ede835e7eceb5b2396e361794bea98e3d83a9
Dima Mezhevich <octadim@gmail.com>
2024-02-14 13:46:20 +0300

    +feat: Added Meta data into the class User

--- apps/api/user/models.py
+++ apps/api/user/models.py
@@ -66,6 +66,12 @@ class User(AbstractBaseUser, PermissionsMixin):  # todo: Try later to rename Cus
 
     objects = UserManager()
 
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ["email", "username", "nickname"]
+
+
     def __str__(self):
         return f"User: {self.username} [ nickname: {self.nickname}, email: {self.email} ]"
 
