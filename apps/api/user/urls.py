b933210f6a176c9dff9e195192b19e9b84eace26
Dima Mezhevich <octadim@gmail.com>
2024-02-17 03:15:22 +0300

    +trial urls: Added url for standard view and trial working url (creating trainer user) with two options of assigning (changing) value of the field differ from the model default value and serializer's form

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -3,9 +3,11 @@ from apps.api.user.views import (ListAllUsersGenericList,
                                  RegisterNewUserGenericCreate,
                                  RegisterNewSuperUserGenericCreate,
                                  RegisterNewStaffUserGenericCreate,
+                                 RegisterNewTrainerUserGenericCreate,
                                  _Test_RegisterNewUserGenericCreate,
                                  _Test_RegisterNewSuperUserGenericCreate,
                                  _Test_RegisterNewStaffUserGenericCreate,
+                                 _Test_RegisterNewTrainerUserGenericCreate,
                                  )
 
 app_name = "user"
@@ -13,18 +15,19 @@ app_name = "user"
 urlpatterns = [
     # superuser
     path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
-
-    # user, staff, superuser
-    path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
-    # staff, superuser
-    path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
-    # superuser
     path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
+
+    # staff, superuser
+    path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
+    path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
+    path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
+    path("register_new_trainer_user/", RegisterNewTrainerUserGenericCreate.as_view(), name="register-new-trainer-user"),
 
     # ################### MY TRIAL CODE ################################
     path("_test_register_new_user_v2/", _Test_RegisterNewUserGenericCreate.as_view(), name="test-register-new-user-v2"),
-    path("_test_register_new_superuser_v2/", _Test_RegisterNewSuperUserGenericCreate.as_view(), name="test-register-new-superuser-v2"),
     path("_test_register_new_staff_user_v2/", _Test_RegisterNewStaffUserGenericCreate.as_view(), name="test-register-new-staff-user-2"),
+    path("_test_register_new_trainer_user_v2/", _Test_RegisterNewTrainerUserGenericCreate.as_view(), name="test-register-new-trainer-user-v2"),
+    path("_test_register_new_superuser_v2/", _Test_RegisterNewSuperUserGenericCreate.as_view(), name="test-register-new-superuser-v2"),
     # ##################################################################
 
 ]
--- apps/api/user/views.py
+++ apps/api/user/views.py
@@ -19,7 +19,9 @@ from apps.api.messages import (NO_USERS_MSG,
                                SUPERUSER_CREATED_MSG,
                                SUPERUSER_NOT_CREATED_MSG,
                                STAFF_USER_CREATED_MSG,
-                               STAFF_USER_NOT_CREATED_MSG
+                               STAFF_USER_NOT_CREATED_MSG,
+                               TRAINER_USER_CREATED_MSG,
+                               TRAINER_USER_NOT_CREATED_MSG
                                )
 
 from apps.api.messages_errors import (NOT_SUPERUSER_FORBIDDEN,
@@ -28,17 +30,16 @@ from apps.api.messages_errors import (NOT_SUPERUSER_FORBIDDEN,
 
 # ##################### TRIAL CODE #####################################
 from apps.api.user.serializers_test_any_user_reg import (
-                                      _Test_UserRegistrySerializer,
-                                      _Test_SuperUserRegistrySerializer,
-                                      _Test_StaffUserRegistrySerializer)
+                                    _Test_UserRegistrySerializer,
+                                    _Test_SuperUserRegistrySerializer,
+                                    _Test_StaffUserRegistrySerializer,
+                                    _Test_TrainerUserRegistrySerializer)
 # ######################################################################
 
 from apps.api.user.serializer_user_reg import UserRegistrySerializer
 from apps.api.user.serializer_superuser_reg import SuperUserRegistrySerializer
 from apps.api.user.serializer_staff_user_reg import StaffUserRegistrySerializer
-
-
-
+from apps.api.user.serializer_trainer_user_reg import TrainerUserRegistrySerializer
 
 from apps.api.user.serializers import AllUsersSerializer
 
@@ -136,6 +137,29 @@ class RegisterNewStaffUserGenericCreate(CreateAPIView):
                       "data":cerializer.errors})
 
 
+class RegisterNewTrainerUserGenericCreate(CreateAPIView):
+    serializer_class = TrainerUserRegistrySerializer
+
+    def post(self, request, *args, **kwargs):
+
+        # user_is_staff = request.user.is_staff  # Commented, because will be defined by Permissions classes
+        # if not user_is_staff:
+        #     return Response(
+        #         status=status.HTTP_403_FORBIDDEN,
+        #         data={"message":gettext_lazy(NOT_STAFF_USER_FORBIDDEN)})
+
+        cerializer = self.serializer_class(data=request.data)
+        if cerializer.is_valid():
+            cerializer.save()
+            return Response(
+                    status=status.HTTP_200_OK,
+                    data={"message":gettext_lazy(TRAINER_USER_CREATED_MSG),
+                          "data":cerializer.data})
+
+        return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"message":gettext_lazy(TRAINER_USER_NOT_CREATED_MSG),
+                      "data":cerializer.errors})
 
 
 
@@ -190,16 +214,16 @@ class _Test_RegisterNewSuperUserGenericCreate(CreateAPIView):
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_200_OK,
-                            data={"message":gettext_lazy(USER_CREATED_MSG),
+                            data={"message":gettext_lazy(SUPERUSER_CREATED_MSG),
                                   "data":serializer.data})
 
         return Response(status=status.HTTP_400_BAD_REQUEST,
-                        data={"message":gettext_lazy(USER_NOT_CREATED_MSG),
+                        data={"message":gettext_lazy(SUPERUSER_NOT_CREATED_MSG),
                               "data":serializer.errors})
 
 
 class _Test_RegisterNewStaffUserGenericCreate(CreateAPIView):
-    serializer_class = _Test_StaffUserRegistrySerializer
+    serializer_class = _Test_TrainerUserRegistrySerializer
 
     def post(self, request: Request, *args, **kwargs):
         # req_data = dict(request.data)  # Option 3: Not recommended, better in serializer. Not for custom manager
@@ -214,11 +238,35 @@ class _Test_RegisterNewStaffUserGenericCreate(CreateAPIView):
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_200_OK,
-                            data={"message":gettext_lazy(USER_CREATED_MSG),
+                            data={"message":gettext_lazy(STAFF_USER_CREATED_MSG),
                                   "data":serializer.data})
 
         return Response(status=status.HTTP_400_BAD_REQUEST,
-                        data={"message":gettext_lazy(USER_NOT_CREATED_MSG),
+                        data={"message":gettext_lazy(STAFF_USER_NOT_CREATED_MSG),
+                              "data":serializer.errors})
+
+
+class _Test_RegisterNewTrainerUserGenericCreate(CreateAPIView):
+    serializer_class = _Test_TrainerUserRegistrySerializer
+
+    def post(self, request: Request, *args, **kwargs):
+        # req_data = dict(request.data)  # Option 3: Not recommended, better in serializer. Not for custom manager
+        # req_data_dict = {fld: val[0] for fld, val in req_data.items() if val[0] != ""}
+        # _Test_StaffUserRegistrySerializer.Meta.fields.append("is_staff")
+        # _Test_StaffUserRegistrySerializer.Meta.fields.append("is_superuser")
+        # req_data_dict["is_staff"] = "True"
+        # req_data_dict["is_superuser"] = "False"
+        # serializer = self.serializer_class(data=req_data_dict)
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK,
+                            data={"message":gettext_lazy(TRAINER_USER_CREATED_MSG),
+                                  "data":serializer.data})
+
+        return Response(status=status.HTTP_400_BAD_REQUEST,
+                        data={"message":gettext_lazy(TRAINER_USER_NOT_CREATED_MSG),
                               "data":serializer.errors})
 
 # ######################################################################
