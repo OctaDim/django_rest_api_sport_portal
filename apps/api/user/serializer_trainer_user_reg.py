a02afee12036c615d99f71e12d8fcdfd157d6d80
Dima Mezhevich <octadim@gmail.com>
2024-02-17 03:04:42 +0300

    +feat: Added serializer 'TrainerUserRegistrySerializer' similar to the standard method commonly used in practice

---
+++ apps/api/user/serializer_trainer_user_reg.py
@@ -0,0 +1,27 @@
+from apps.api.user.serializer_user_reg import UserRegistrySerializer
+
+# ##################################################################
+# from django.contrib.auth.models import User  # Django default user
+from apps.api.user.models import User  # Custom user
+# ##################################################################
+
+
+
+class TrainerUserRegistrySerializer(UserRegistrySerializer):  # Inherited from the ordinal user serializer
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        user = User.objects.create_trainer_user(
+                        # email=validated_data.get("email"),  # Refactored. Passing as **validated_data dictionary,
+                        # username=validated_data.get("username"),  # instead of huge quantity of the named parameters
+                        # nickname=validated_data.get("nickname"),
+                        # first_name=validated_data.get("first_name"),
+                        # last_name=validated_data.get("last_name"),
+                        # phone=validated_data.get("phone"),
+                        # password=validated_data.get("password"),
+                        # is_staff=validated_data.get("is_staff"),
+                        # is_superuser=validated_data.get("is_superuser"),
+                        # is_verified=validated_data.get("is_verified"),
+                        **validated_data
+                        )
+        return user
