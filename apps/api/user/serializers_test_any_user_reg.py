b33c53ab2c63be06ff68194a53c25ca5b7a71447
Dima Mezhevich <octadim@gmail.com>
2024-02-17 03:10:52 +0300

    +trial serializer: Added trial working serializer (creating trainer user) with two options of assigning (changing) value of the field differ from the model default value and serializer's form

--- apps/api/user/serializers_test_any_user_reg.py
+++ apps/api/user/serializers_test_any_user_reg.py
@@ -48,6 +48,7 @@ class _Test_SuperUserRegistrySerializer(UserRegistrySerializer):  # Inherited fr
             **validated_data)
         return user
 
+
 class _Test_StaffUserRegistrySerializer(UserRegistrySerializer):  # Inherited from the ordinal user serializer
     def validate(self, attrs):  # Option 1: To assign value into the fields set default in the Model
         super().validate(attrs)  # Completion of all checks defined in custom UserRegistrySerializer (can be abstract)
@@ -64,6 +65,25 @@ class _Test_StaffUserRegistrySerializer(UserRegistrySerializer):  # Inherited fr
                 # is_verified=False,  # Can be defined directly here or in validate method
                 **validated_data)
         return user
+
+
+class _Test_TrainerUserRegistrySerializer(UserRegistrySerializer):  # Inherited from the ordinal user serializer
+    def validate(self, attrs):  # Option 1: To assign value into the fields set default in the Model
+        super().validate(attrs)  # Completion of all checks defined in custom UserRegistrySerializer (can be abstract)
+        attrs["is_trainer"] = "True"  # Redefined default value (if determined in the model fields parameters)
+        attrs["is_staff"] = "True"  # Redefined default value (if determined in the model fields parameters)
+        # attrs["is_verified"] = "False"  # Redefined default value (if determined in the model fields parameters)
+        return attrs
+
+    def create(self, validated_data):  # Option 2: To assign value into the fields set default in the Model
+        validated_data.pop("password2")  # Refactored to avoid tedious getting values from validated_data via get()
+        user = User.objects._test_create_any_type_user(
+                # is_staff=True,  # Can be defined directly here or in validate method
+                # is_superuser=False,  # Can be defined directly here or in validate method
+                # is_verified=False,  # Can be defined directly here or in validate method
+                **validated_data)
+        return user
+
 # ######################################################################
 # ######################################################################
 # ######################################################################
