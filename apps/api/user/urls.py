5da6127f3eb805c61f3867eb81a09691d7a443c1
Dima Mezhevich <octadim@gmail.com>
2024-02-18 03:11:52 +0300

    +url: Added url to the view with xRUD user model actions for the superusers

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -4,6 +4,7 @@ from apps.api.user.views import (ListAllUsersGenericList,
                                  RegisterNewSuperUserGenericCreate,
                                  RegisterNewStaffUserGenericCreate,
                                  RegisterNewTrainerUserGenericCreate,
+                                 UserInfoByIdGenericRetrieveUpdDestroy,
                                  _Test_RegisterNewUserGenericCreate,
                                  _Test_RegisterNewSuperUserGenericCreate,
                                  _Test_RegisterNewStaffUserGenericCreate,
@@ -14,14 +15,14 @@ app_name = "user"
 
 urlpatterns = [
     # superuser
-    path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
     path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
+    path("user_info_by_id_hard/<int:user_id>/", UserInfoByIdGenericRetrieveUpdDestroy.as_view(), name="get-user-info-by-id-hard"),
 
     # staff, superuser
     path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
     path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
-    path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
     path("register_new_trainer_user/", RegisterNewTrainerUserGenericCreate.as_view(), name="register-new-trainer-user"),
+    path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
 
     # ################### MY TRIAL CODE ################################
     path("_test_register_new_user_v2/", _Test_RegisterNewUserGenericCreate.as_view(), name="test-register-new-user-v2"),
