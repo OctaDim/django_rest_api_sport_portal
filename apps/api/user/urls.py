d7b6d764fd7fc328847b07bdcb2e3daf06c7aca3
Dima Mezhevich <octadim@gmail.com>
2024-02-19 23:17:56 +0300

    +urls: Added urls with user info with hard delete (superuser) and soft delete (staff)

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from apps.api.user.views import (ListAllUsersGenericList,
+from apps.api.user.views import (AllUsersGenericListForStaff,
+                                 AllUsersGenericListForSuperuser,
                                  RegisterNewUserGenericCreate,
                                  RegisterNewSuperUserGenericCreate,
                                  RegisterNewStaffUserGenericCreate,
                                  RegisterNewTrainerUserGenericCreate,
                                  UserInfoByIdGenericRetrieveUpdDestroy,
+                                 UserInfoByIdGenericRetrieveUpdate,
                                  _Test_RegisterNewUserGenericCreate,
                                  _Test_RegisterNewSuperUserGenericCreate,
                                  _Test_RegisterNewStaffUserGenericCreate,
@@ -16,13 +18,15 @@ app_name = "user"
 urlpatterns = [
     # superuser
     path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
-    path("user_info_by_id_hard/<int:user_id>/", UserInfoByIdGenericRetrieveUpdDestroy.as_view(), name="get-user-info-by-id-hard"),
+    path("list_all_users_all_fields/", AllUsersGenericListForSuperuser.as_view(), name="list-all-users-all-fields"),
+    path("user_info_by_id_hard_del/<int:user_id>/", UserInfoByIdGenericRetrieveUpdDestroy.as_view(), name="user-info-by-id-hard-del"),
 
     # staff, superuser
     path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
     path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
     path("register_new_trainer_user/", RegisterNewTrainerUserGenericCreate.as_view(), name="register-new-trainer-user"),
-    path("list_all_users/", ListAllUsersGenericList.as_view(), name="list-all-users"),
+    path("list_all_users_limit_fields/", AllUsersGenericListForStaff.as_view(), name="list-all-users-limit-fields"),
+    path("user_info_by_id_soft_del/<int:user_id>/", UserInfoByIdGenericRetrieveUpdate.as_view(), name="user-info-by-id-soft-del"),
 
     # ################### MY TRIAL CODE ################################
     path("_test_register_new_user_v2/", _Test_RegisterNewUserGenericCreate.as_view(), name="test-register-new-user-v2"),
