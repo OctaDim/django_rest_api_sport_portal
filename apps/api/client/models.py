2f93e7f8bcb40580c015f608c4fd33b8c6b76a98
Dima Mezhevich <octadim@gmail.com>
2024-02-20 22:37:12 +0300

    +model: Added model ClientStatus

--- apps/api/client/models.py
+++ apps/api/client/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
 
-# Create your models here.
+from apps.api.messages_fields import (CLIENT_STATUS,
+                                      CLIENT_STATUSES,
+                                      CREATED_AT,
+                                      UPDATED_AT,
+                                      CREATOR,
+                                      )
+
+from apps.api.user.models import User
+
+
+
+class ClientStatus(models.Model):
+    name = models.CharField(max_length=30,
+                            verbose_name=gettext_lazy(CLIENT_STATUS))
+
+    created_at = models.DateTimeField(auto_now_add=True,
+                                        verbose_name=gettext_lazy(CREATED_AT))
+
+    updated_at = models.DateTimeField(auto_now=True,
+                                        verbose_name=gettext_lazy(UPDATED_AT))
+
+    creator = models.ForeignKey(User,
+                                on_delete=models.PROTECT,
+                                verbose_name=gettext_lazy(CREATOR))
+
+    class Meta:
+        verbose_name = gettext_lazy(CLIENT_STATUS)
+        verbose_name_plural = gettext_lazy(CLIENT_STATUSES)
+        ordering = ["name"]
