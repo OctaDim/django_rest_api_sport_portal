579809d7ff2fc94a84de6859777e7146b2a2b615
Dima Mezhevich <octadim@gmail.com>
2024-02-20 22:20:33 +0300

    +msgs variables: Added outer messages variables

--- apps/api/messages_fields.py
+++ apps/api/messages_fields.py
@@ -1,6 +1,22 @@
-EMAIL="email"
-USERNAME="username"
-NICKNAME="nickname"
-FIRST_NAME="first name"
-LAST_NAME="last name"
-PHONE="phone"
+CREATOR = "creator"
+
+EMAIL = "email"
+USERNAME = "username"
+NICKNAME = "nickname"
+FIRST_NAME = "first name"
+LAST_NAME = "last name"
+PHONE = "phone"
+
+IS_STAFF = "is staff"
+IS_SUPERUSER = "is superuser"
+IS_TRAINER = "is trainer"
+IS_VERIFIED = "is verified"
+IS_ACTIVE = "is active"
+DATE_JOINED = "joined first date"
+LAST_LOGIN = "last login date"
+
+CREATED_AT = "created date"
+UPDATED_AT = "updated date"
+
+CLIENT_STATUS = "client status"
+CLIENT_STATUSES = "client statuses"
