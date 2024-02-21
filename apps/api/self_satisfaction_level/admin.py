c3ef58fb246012ef00de34af361f0f6f9cb59a5f
Dima Mezhevich <octadim@gmail.com>
2024-02-21 16:11:48 +0300

    +admin panel model: Added admin model SelfSatisfactionLevelAdmin with list, filter and search fields definition

--- apps/api/self_satisfaction_level/admin.py
+++ apps/api/self_satisfaction_level/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
-
 # Register your models here.
+
+from apps.api.self_satisfaction_level.models import SelfSatisfactionLevel
+
+@admin.register(SelfSatisfactionLevel)  # Option 1
+# admin.site.register(SelfSatisfactionLevel)  # Option 2
+class SelfSatisfactionLevelAdmin(admin.ModelAdmin):
+    list_display = ["id",
+                    "icon",
+                    "value",
+                    "name",
+                    "description",
+                    "created_at",
+                    "updated_at",
+                    "creator",
+                    ]
+
+    list_filter = ["id",
+                   "icon",
+                   "value",
+                   "name",
+                   "description",
+                   "created_at",
+                   "updated_at",
+                   "creator",
+                   ]
+
+    search_fields = ["id",
+                     "icon",
+                     "value",
+                     "name",
+                     "description",
+                     "created_at",
+                     "updated_at",
+                     "creator",
+                    ]
+
+
+
+    ###### POSSIBLE OPTIONS #####
+    # list_display = ("__str__",)
+    # list_display_links = ()
+    # list_filter = ()
+    # list_select_related = False
+    # list_per_page = 100
+    # list_max_show_all = 200
+    # list_editable = ()
+    # search_fields = ()
+    # search_help_text = None
+    # date_hierarchy = None
+    # save_as = False
+    # save_as_continue = True
+    # save_on_top = False
+    # paginator = Paginator
+    # preserve_filters = True
+    # inlines = ()
