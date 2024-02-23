b0fae43a94d7e629ce317925c3f3a000288b4c3f
Dima Mezhevich <octadim@gmail.com>
2024-02-23 05:19:31 +0300

    +feat, +fix: Added resizing of the icon image via ResizedImageField (and optionally via Pillow lib - commented) and fixed path forming for the image saving. Magic method __str__ was added

--- apps/api/self_satisfaction_level/models.py
+++ apps/api/self_satisfaction_level/models.py
@@ -14,14 +14,40 @@ from apps.api.messages_fields import (SATISFACTION_LEVEL,
 
 from apps.api.user.models import User
 
+from django_resized import ResizedImageField
+import os
+from time import strftime  # Optional, if necessary
+from PIL import Image  # Optional, if necessary
+
+
+
+def get_image_file_name(instance, filename):  # Option 1, more simple: instead of PIL (pillow) lib (see bellow)
+    path, ext = os.path.splitext(filename)
+    level_value = f"{instance.value :04d}"
+    # add_date = strftime("%Y_%m_%d")  # Optional, if necessary
+
+    file_name = f"self_satisfaction_level_{level_value}_icon{ext}"
+    full_path_file_name = os.path.join("api",
+                                       "self_satisfaction_level",
+                                       "img",
+                                       "icon",
+                                       file_name)
+    return full_path_file_name
+
 
 
 class SelfSatisfactionLevel(models.Model):
-    icon = models.ImageField(name="self satisfaction level icon",
-                             width_field=30,
-                             height_field=30,
-                             blank=True, null=True,
-                             verbose_name=gettext_lazy(SATISFACTION_LEVEL_ICON))
+    icon_link = ResizedImageField(  # Option 1, more simple: instead of PIL (pillow) lib (see bellow)
+                            upload_to=get_image_file_name,
+                            size=[150,150],
+                            quality=100,
+                            blank=True, null=True,
+                            verbose_name=gettext_lazy(SATISFACTION_LEVEL_ICON))
+
+    # test_pillow_icon_link = models.ImageField(  # Option 2, more advanced: to resize image via PIL (pillow) lib
+    #                         upload_to="pillow_library_test/",
+    #                         blank=True, null=True,
+    #                         verbose_name=gettext_lazy("Test Pillow Library"))
 
     value = models.SmallIntegerField(
                             unique=True,
@@ -49,3 +75,15 @@ class SelfSatisfactionLevel(models.Model):
         verbose_name = gettext_lazy(SATISFACTION_LEVEL)
         verbose_name_plural = gettext_lazy(SATISFACTION_LEVELS)
         ordering = ["value"]
+
+
+    def __str__(self):
+        return f"{self.name} (id={self.pk})"
+
+
+    # def save(self, *args, **kwargs):  # Option 2, more advanced: to resize image via PIL (pillow) lib
+    #     super().save(*args, **kwargs)
+    #     with Image.open(self.test_pillow_icon_link.path) as img:  # PIL.Image.open()
+    #         target_width, target_height = 300, 150  # Setting new image dimensions
+    #         img = img.resize((target_width, target_height))
+    #         img.save(self.test_pillow_icon_link.path, quality=100)
