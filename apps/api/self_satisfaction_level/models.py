73788f12ea0aff576008b687b822ef8d151e39ee
Dima Mezhevich <octadim@gmail.com>
2024-02-24 21:08:53 +0300

    minor renaming of the verbose name message for better explicity

--- apps/api/self_satisfaction_level/models.py
+++ apps/api/self_satisfaction_level/models.py
@@ -5,7 +5,7 @@ from apps.api.messages_fields import (SATISFACTION_LEVEL,
                                       SATISFACTION_LEVELS,
                                       SATISFACTION_LEVEL_NAME,
                                       SATISFACTION_LEVEL_VALUE,
-                                      SATISFACTION_LEVEL_ICON,
+                                      SATISFACTION_LEVEL_ICON_LINK,
                                       DESCRIPTION,
                                       CREATED_AT,
                                       UPDATED_AT,
@@ -22,11 +22,11 @@ from apps.api.self_satisfaction_level.functions import get_image_file_name
 
 class SelfSatisfactionLevel(models.Model):
     icon_link = ResizedImageField(  # Option 1, more simple: instead of PIL (pillow) lib (see bellow)
-                            upload_to=get_image_file_name,
-                            size=[150,150],
-                            quality=100,
-                            blank=True, null=True,
-                            verbose_name=gettext_lazy(SATISFACTION_LEVEL_ICON))
+                    upload_to=get_image_file_name,
+                    size=[150,150],
+                    quality=100,
+                    blank=True, null=True,
+                    verbose_name=gettext_lazy(SATISFACTION_LEVEL_ICON_LINK))
 
     # test_pillow_icon_link = models.ImageField(  # Option 2, more advanced: to resize image via PIL (pillow) lib
     #                         upload_to="pillow_library_test/",
