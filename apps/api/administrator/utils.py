aed13b3042574052974b666167e1acbe9f028762
Dima Mezhevich <octadim@gmail.com>
2024-03-02 17:25:19 +0300

    +functions: Often repeated ops (deleting img file via default storage and via os.remove) were taken outside into separate functions. Func get_image_file_name was corrected, to fix none index.id error, return img name was changed

--- apps/api/administrator/utils.py
+++ apps/api/administrator/utils.py
@@ -1,22 +1,56 @@
 import os
 import pathlib
-from time import strftime  # Optional, if necessary
-from PIL import Image  # Optional, if necessary
-from datetime import datetime, UTC # Optional, if necessary
+from datetime import datetime, UTC
+
+from django.core.files.storage import default_storage
+
+from django.utils.translation import gettext_lazy
+from apps.api.messages_technical import ERROR_OCCURRED
+
+
+
+
 
 
 def get_image_file_name(instance, filename):
-    id = f"{instance.id :04d}"
+    if instance.id:
+       instance_id = f"{instance.id :05d}"
+    else:
+        instance_id = None
+
+    instance_nickname = instance.user.nickname.lower()
+    image_update_date = datetime.now(UTC).strftime("%Y_%m_%d")
+
+
     file_ext = pathlib.Path(filename).suffix
+    # file_path, file_ext = os.path.splitext(filename)  # Optional, if necessary both path and ext
 
-    # file_path, file_ext = os.path.splitext(filename)       # Optional, if necessary both path and ext
-    # instance_name = instance.name                          # Optional, if necessary
-    # img_add_date = datetime.now(UTC).strftime("%Y_%m_%d")  # Optional, if necessary
 
-    file_name = f"administrator_avatar_{id}_thumbnail{file_ext}"
+    file_name = (f"administrator_id_{instance_id}_"
+                 f"upd_{image_update_date}_"
+                 f"thumbnail"
+                 f"{file_ext}")
+
     full_path_file_name = os.path.join("api",
                                        "administrator",
                                        "img",
                                        "thumbnail",
                                        file_name)
     return full_path_file_name
+
+
+def delete_file_default_storage(project_path_file_name):
+    try:
+        if default_storage.exists(project_path_file_name):  # for cloud default_storage
+            default_storage.delete(project_path_file_name)  # for cloud default_storage
+    except (IOError, ValueError) as error:
+        print(gettext_lazy(ERROR_OCCURRED(error)))
+
+
+
+def delete_file_os_remove(project_path_file_name):
+    try:
+        if os.path.exists(project_path_file_name):  # only for local storage
+            os.remove(project_path_file_name)       # only for local storage
+    except (IOError, ValueError) as error:
+        print(gettext_lazy(ERROR_OCCURRED(error)))
