4c19cff651e088b176d8677e5f9a537c1ef968d2
Dima Mezhevich <octadim@gmail.com>
2024-03-02 17:47:42 +0300

    +signals: Signals were attached to the apps AdministratorConfig

--- apps/api/administrator/apps.py
+++ apps/api/administrator/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AdministratorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.api.administrator'
+
+    def ready(self):
+        import apps.api.administrator.signals
