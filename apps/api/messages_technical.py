cfc1e4c9c48f69a921cd2cb01cb90ac857d6da2c
Dima Mezhevich <octadim@gmail.com>
2024-03-06 23:39:55 +0300

    +msgs variables: Added or edited outer messages variables

--- apps/api/messages_technical.py
+++
@@ -1,5 +0,0 @@
-def EXCEPTION_INFO(exception_error: str) -> str:
-    return f"\t##### FORESEEN EXCEPTION INFO: {exception_error}"
-
-def ERROR_OCCURRED(exception_error: str) -> str:
-    return f"\t##### ERROR OCCURRED: {exception_error}"
