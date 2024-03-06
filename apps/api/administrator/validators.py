59ec9c3f6d7725fa65e8c153a70c7935a624320c
Dima Mezhevich <octadim@gmail.com>
2024-03-06 23:40:41 +0300

    + validator: Image size validator was defined

---
+++ apps/api/administrator/validators.py
@@ -0,0 +1,14 @@
+from django.utils.translation import gettext_lazy
+from django.core.exceptions import ValidationError
+from apps.api.administrator.utils import number_or_str_to_float
+
+from apps.api.administrator.settings import MAX_IMAGE_FILE_SIZE_IN_MB
+from apps.api.messages_errors import MAXIMUM_FILE_SIZE
+
+
+def validate_image_size(image):
+    file_size = image.file.size
+    max_file_size = number_or_str_to_float(MAX_IMAGE_FILE_SIZE_IN_MB)
+    if file_size > max_file_size * 1024 * 1024:
+        raise ValidationError(
+            gettext_lazy(MAXIMUM_FILE_SIZE(max_file_size)))
