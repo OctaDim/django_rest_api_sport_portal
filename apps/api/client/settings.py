ca62e32490fdf0230ca89e1a963e8c00a1da38fe
Dima Mezhevich <octadim@gmail.com>
2024-03-07 21:14:02 +0300

    + outer application settings: Module with application settings was added (image resizing settings, when savin via admin panel)

---
+++ apps/api/client/settings.py
@@ -0,0 +1,15 @@
+# Maximum image file size in Megabytes:
+# float, integer or string with float|integer:
+MAX_IMAGE_FILE_SIZE_IN_MB = 10.0
+
+# Avatar width in pixels after resizing:
+# integer or string with integer (if float -> will be truncated to integer)
+AVATAR_WIDTH = 150
+
+# Avatar height in pixels after resizing:
+# integer or string with integer (if float -> will be truncated to integer)
+AVATAR_HEIGHT = 150
+
+# Image resampling quality when resizing:
+# integer or string with integer (if float -> will be truncated to integer)
+IMAGE_QUALITY = 100
