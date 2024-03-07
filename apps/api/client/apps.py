11ffa998e4c1ebdb6cb93c59e186dd333f9ca24c
Dima Mezhevich <octadim@gmail.com>
2024-03-07 05:30:03 +0300

    +signals: Signals were attached to the apps ClientConfig

--- apps/api/client/apps.py
+++ apps/api/client/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ClientConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.api.client'
+
+    def ready(self):
+        import apps.api.client.signals
