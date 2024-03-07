b2d333d0e82951797d1d0511c6aa8e6bb4b57752
Dima Mezhevich <octadim@gmail.com>
2024-03-07 03:12:05 +0300

    Revert "+ utils functions: File creation name was changed in get_image_file_name(). Added functions: number_or_str_to_int() and number_or_str_to_float() to read values from the outer app settings file"
    This reverts commit 574382967cb8bfe63bfc8207fbfb01b2c4c871cb.

--- apps/api/administrator/utils.py
+++ apps/api/administrator/utils.py
@@ -5,29 +5,26 @@ from datetime import datetime, UTC
 from django.core.files.storage import default_storage
 
 from django.utils.translation import gettext_lazy
-from apps.api.messages_non_front import (ERROR_OCCURRED,
-                                         EXCEPTION_INFO)
+from apps.api.messages_technical import ERROR_OCCURRED
 
-from django.core.exceptions import ValidationError
+
 
 
 
 
 def get_image_file_name(instance, filename):
-    instance_id = None
-    try:
-        if instance.id:
-           instance_id = f"{instance.id :05d}"
-    except instance.DoesNotExist as error:
-        print(gettext_lazy(EXCEPTION_INFO(error)))
+    if instance.id:
+       instance_id = f"{instance.id :05d}"
+    else:
+        instance_id = None
 
-
-
-    # instance_nickname = instance.user.nickname.lower()
+    instance_nickname = instance.user.nickname.lower()
     image_update_date = datetime.now(UTC).strftime("%Y_%m_%d")
+
 
     file_ext = pathlib.Path(filename).suffix
     # file_path, file_ext = os.path.splitext(filename)  # Optional, if necessary both path and ext
+
 
     file_name = (f"administrator_id_{instance_id}_"
                  f"upd_{image_update_date}_"
@@ -50,25 +47,10 @@ def delete_file_default_storage(project_path_file_name):
         print(gettext_lazy(ERROR_OCCURRED(error)))
 
 
+
 def delete_file_os_remove(project_path_file_name):
     try:
         if os.path.exists(project_path_file_name):  # only for local storage
             os.remove(project_path_file_name)       # only for local storage
     except (IOError, ValueError) as error:
         print(gettext_lazy(ERROR_OCCURRED(error)))
-
-
-def number_or_str_to_int(number_or_str_number: int | float | str) -> int | None:
-    try:
-        integer_number = abs(int(number_or_str_number))
-        return integer_number
-    except (TypeError, ValueError) as error:
-        raise ValidationError(gettext_lazy(ERROR_OCCURRED(error)))
-
-
-def number_or_str_to_float(number_or_str_number: int | float | str) -> float | None:
-    try:
-        float_number = abs(float(number_or_str_number))
-        return float_number
-    except (TypeError, ValueError) as error:
-        raise ValidationError(gettext_lazy(ERROR_OCCURRED(error)))
