2951ba53e3f1225026961a82356f665f5b888ff6
Dima Mezhevich <octadim@gmail.com>
2024-03-08 01:23:54 +0300

    +signals: Signals were attached to the apps CoachConfig

--- apps/api/coach/apps.py
+++ apps/api/coach/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoachConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.api.coach'
+
+    def ready(self):
+        import apps.api.coach.signals
