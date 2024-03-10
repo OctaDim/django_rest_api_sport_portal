2648d0b0c79ab1b3e9abde02bcea9c64ad409715
Dima Mezhevich <octadim@gmail.com>
2024-03-10 16:18:19 +0300

    +app: Django application was added 'training_group'

--- apps/api/training_group/apps.py
+++ apps/api/training_group/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EnrollGroupConfig(AppConfig):
+class TrainingGroupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'enroll_group'
+    name = 'apps.api.training_group'
