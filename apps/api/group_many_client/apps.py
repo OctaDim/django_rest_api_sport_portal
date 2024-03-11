308198d8ff8d8c71de4a5eb53522833d64fc0117
Dima Mezhevich <octadim@gmail.com>
2024-03-11 20:20:17 +0300

    +app: Application 'group_many_client' was initialized to place intermediate model with many-to-many references

--- apps/api/group_many_client/apps.py
+++ apps/api/group_many_client/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GroupClientConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.api.group_client'
+    name = 'group_many_client'
