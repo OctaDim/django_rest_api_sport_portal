777823639234f1ae5ef622caefdd11da91b28dc6
Dima Mezhevich <octadim@gmail.com>
2024-03-11 20:20:51 +0300

    +app: Application 'group_many_coach' was initialized to place intermediate model with many-to-many references

--- apps/api/group_many_coach/apps.py
+++ apps/api/group_many_coach/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EnrollGroupConfig(AppConfig):
+class GroupCoachConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'enroll_group'
+    name = 'group_many_coach'
