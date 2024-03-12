7c447a76edab4f24859a96de27072c27a74cc4d8
Dima Mezhevich <octadim@gmail.com>
2024-03-12 13:15:00 +0300

    +app: Application 'group_client_progress' was initialized (renamed)

--- apps/api/group_client_progress/apps.py
+++ apps/api/group_client_progress/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GroupClientProgressDataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.api.group_client_progress_data'
+    name = 'apps.api.group_client_progress'
