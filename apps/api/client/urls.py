599ae733d37d72401918479c2a8667fd134f2943
Dima Mezhevich <octadim@gmail.com>
2024-03-15 04:51:45 +0300

    +urls: all_clients/ and create_new_client/

--- apps/api/client/urls.py
+++ apps/api/client/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path
 
-# from apps.api.administrator.views import
+from apps.api.client.views import (AllClientsGenericList,
+                                   CreateNewClientGenericCreate,
+                                   )
 
 app_name = "clients"
 
 urlpatterns = [
+   path("all_clients/",
+         AllClientsGenericList.as_view(),
+         name="list_all-clients"),
 
+    path("create_new_client/",
+         CreateNewClientGenericCreate.as_view(), name="create-new-client"),
+
+    # path("client_by_id_soft/<int:client_id>/",
+    #      ClientByIdGenericRetrieveUpd.as_view(),
+    #      name="client-by-id-soft"),
+    #
+    # path("client_by_id_hard/<int:user_id>/",
+    #      ClientByIdGenericRetrieveUpdDestroy.as_view(),
+    #      name="client-by-id-hard"),
 ]
