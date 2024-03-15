bbbcf52d8f3127f4fd7abad9c5274d1d651292fc
Dima Mezhevich <octadim@gmail.com>
2024-03-15 14:09:23 +0300

    refactoring: Image size validators repeating code was transferred to utils

--- apps/api/utils/utils.py
+++ apps/api/utils/utils.py
@@ -6,7 +6,21 @@ from django.utils.translation import gettext_lazy
 from apps.api.messages_api.messages_non_front import (ERROR_OCCURRED)
 
 from django.core.exceptions import ValidationError
+from apps.api.messages_api.messages_non_front import EXCEPTION_INFO
+from apps.api.messages_api.messages_errors import MAXIMUM_FILE_SIZE
 
+
+
+def check_maximum_limit_image_file_size(image, max_image_file_size_in_mb: float|int) -> None:
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ IMAGE LIMIT", max_image_file_size_in_mb)
+    try:
+        file_size = image.file.size
+        max_file_size = number_or_str_to_abs_float(max_image_file_size_in_mb)
+        if file_size > max_file_size * 1024 * 1024:
+            raise ValidationError(
+                gettext_lazy(MAXIMUM_FILE_SIZE(max_file_size)))
+    except (IOError, Exception) as error:
+        print(gettext_lazy(EXCEPTION_INFO(error)))
 
 
 def delete_file_default_storage(project_path_file_name):
