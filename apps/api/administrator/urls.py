46904bff94109a79d282402b9c083365f02f4581
Dima Mezhevich <octadim@gmail.com>
2024-03-17 14:37:17 +0300

    +url: Administrator (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

--- apps/api/administrator/urls.py
+++ apps/api/administrator/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-# from apps.api.administrator.views import
+from apps.api.administrator.views import (AllAdministratorsGenericList,
+                                   CreateNewAdministratorGenericCreate,
+                                   AdministratorByIdGenericRetrieveUpdate,
+                                   AdministratorByIdGenericRetrieveDestroy,
+                                   )
 
 app_name = "administrators"
 
 urlpatterns = [
+   path("all_administrators/",
+         AllAdministratorsGenericList.as_view(),
+         name="list_all-administrators"),
 
+    path("create_new_administrator/",
+         CreateNewAdministratorGenericCreate.as_view(),
+         name="create-new-administrator"),
+
+    path("administrator_by_id_soft/<int:administrator_id>/",
+         AdministratorByIdGenericRetrieveUpdate.as_view(),
+         name="administrator-by-id-soft"),
+
+    path("administrator_by_id_hard/<int:user_id>/",
+         AdministratorByIdGenericRetrieveDestroy.as_view(),
+         name="administrator-by-id-hard"),
 ]
