664a70e4122db4afd02487a37e7a115173bd827e
Dima Mezhevich <octadim@gmail.com>
2024-03-17 15:02:08 +0300

    +views: Clients (all, create, retrieve by id, update + soft del, retrieve by id + hard del). Soft delete in parent User model

--- apps/api/client/views.py
+++ apps/api/client/views.py
@@ -11,6 +11,7 @@ from apps.api.messages_api.messages_actions import (NO_CLIENTS_MSG,
                                                     ALL_CLIENTS_MSG,
                                                     CLIENT_CREATED_MSG,
                                                     CLIENT_NOT_CREATED_MSG,
+                                                    NO_CLIENT_WITH_ID_MSG,
                                                     CLIENT_DETAILS,
                                                     CLIENT_UPDATED_MSG,
                                                     CLIENT_NOT_UPDATED_MSG,
@@ -26,13 +27,16 @@ from rest_framework.generics import (ListAPIView,
                                      CreateAPIView,
                                      RetrieveUpdateDestroyAPIView,
                                      RetrieveUpdateAPIView,
+                                     RetrieveDestroyAPIView,
                                      )
 
 from apps.api.client.serializers import (ClientAllFieldsModelSerializer,
                                          ClientCreateModelSerializer,
+                                         ClientRetrieveUpdateDeleteModelSerializer,
                                          )
 
 from apps.api.client.models import Client
+from apps.api.user.models import User
 
 
 
@@ -40,8 +44,8 @@ class AllClientsGenericList(ListAPIView):
     serializer_class = ClientAllFieldsModelSerializer
 
     def get_queryset(self):
-        queryset = Client.objects.filter()  # Staff can see all users except superusers
-        return queryset
+        clients = Client.objects.filter()  # Staff can see all users except superusers
+        return clients
 
     def get(self, request: Request, *args, **kwargs):
         clients = self.get_queryset()
@@ -58,17 +62,17 @@ class AllClientsGenericList(ListAPIView):
                               "data": serializer.data}
                         )
 
+
+
 class CreateNewClientGenericCreate(CreateAPIView):
     serializer_class = ClientCreateModelSerializer
+
     def post(self, request: Request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data,
+                                           context={"request":self.request})
 
         if serializer.is_valid():
-            # serializer.validated_data["client_creator"]
             serializer.save()
-            # serializer.save(commit=False)
-            # serializer["client_creator"] = self.request.user
-            # serializer.save
             return Response(status=status.HTTP_200_OK,
                             data={"message": gettext_lazy(CLIENT_CREATED_MSG),
                                   "data": serializer.data})
@@ -76,3 +80,173 @@ class CreateNewClientGenericCreate(CreateAPIView):
         return Response(status=status.HTTP_400_BAD_REQUEST,
                         data={"message": gettext_lazy(CLIENT_NOT_CREATED_MSG),
                               "data": serializer.errors})
+
+
+
+class ClientByIdGenericRetrieveUpdate(RetrieveUpdateAPIView):
+    serializer_class = ClientRetrieveUpdateDeleteModelSerializer
+
+    def get_object(self, *args, **kwargs):
+        client_id = self.kwargs.get("client_id") # Client id from the request additional parameter
+        # user_id = self.request.user.id  # Current user id from the request body
+
+        client_object = get_object_or_404(Client, id=client_id)  # As one dictionary object, with exception
+
+        # client_object = Client.objects.filter(id=client_id).first()  # As one dictionary object, exception should be handled
+        # client_id_object = Client.objects.filter(id=client_id)  # As list with a dictionary element, exception should be handled
+        #
+        # if not client_object:
+        #     return Response(status=status.HTTP_404_NOT_FOUND,
+        #                     data={"message": gettext_lazy(NO_CLIENT_WITH_ID_MSG),
+        #                           "data": {}
+        #                           })
+
+        return client_object
+
+
+    def get(self, request, *args, **kwargs):
+        client = self.get_object()
+
+        serializer=self.serializer_class(instance=client,  # If one dictionary object, got with get_or_404()
+                                         context={"request":self.request})
+        # serializer=self.serializer_class(instance=client,
+        #                                  many=True,
+        #                                  context={"request":self.request})  # If list with one dictionary element, got with filter()
+
+        return Response(status=status.HTTP_200_OK,
+                        data={"message":gettext_lazy(CLIENT_DETAILS),
+                              "data": serializer.data} )
+
+
+    def patch(self, request, *args, **kwargs):  # IMPORTANT: Only if PATCH, entered values stay after validation
+        client = self.get_object()
+
+        client_id = self.kwargs.get("client_id")
+        serializer = self.serializer_class(instance=client,
+                                           data=request.data,
+                                           partial=True,
+                                           context={"client_id": client_id,  # Additionally transferred for serializer validation
+                                                    "request":self.request,
+                                                    }
+                                           )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            user_is_active = serializer.validated_data.get("user_is_active")
+            client_user_id = Client.objects.get(id=client_id).user.id
+            client_user = User.objects.filter(id=client_user_id)
+            if client_user.first().is_active != user_is_active:
+                client_user.update(is_active=user_is_active)
+
+            serializer = self.serializer_class(instance=client,
+                                               data=request.data,
+                                               partial=True,
+                                               context={"client_id": client_id,
+                                                        "request": self.request,
+                                                        }
+                                               )
+            if serializer.is_valid():
+                serializer.save()
+
+            return Response(status=status.HTTP_200_OK,
+                            data={"message":gettext_lazy(CLIENT_UPDATED_MSG),
+                                  "data":serializer.data})
+
+        return Response(status=status.HTTP_400_BAD_REQUEST,
+                        data={"message":gettext_lazy(CLIENT_NOT_UPDATED_MSG),
+                              "data": serializer.errors})
+
+
+    def put(self, request, *args, **kwargs):  # IMPORTANT: Only if PATCH, entered values stay after validation
+        client = self.get_object()
+
+
+        client_id = self.kwargs.get("client_id")
+
+        serializer = self.serializer_class(instance=client,
+                                           data=request.data,
+                                           partial=True,
+                                           context={"client_id": client_id,  # Add transferred for serializer validation
+                                                    "request":self.request,
+                                                    }
+                                           )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            user_is_active = serializer.validated_data.get("user_is_active")
+            client_user_id = Client.objects.get(id=client_id).user.id
+            client_user = User.objects.filter(id=client_user_id)
+            if client_user.first().is_active != user_is_active:
+                client_user.update(is_active=user_is_active)
+
+            serializer = self.serializer_class(instance=client,
+                                               data=request.data,
+                                               partial=True,
+                                               context={"client_id": client_id,
+                                                        "request": self.request,
+                                                        }
+                                               )
+            if serializer.is_valid():
+                serializer.save()
+
+            return Response(status=status.HTTP_200_OK,
+                            data={"message":gettext_lazy(CLIENT_UPDATED_MSG),
+                                  "data":serializer.data})
+
+        return Response(status=status.HTTP_400_BAD_REQUEST,
+                        data={"message":gettext_lazy(CLIENT_NOT_UPDATED_MSG),
+                              "data": serializer.errors})
+
+
+
+class ClientByIdGenericRetrieveDestroy(RetrieveDestroyAPIView):
+    serializer_class = ClientRetrieveUpdateDeleteModelSerializer
+
+    def get_object(self):
+        client_id = self.kwargs.get("client_id") # Client id from the request additional parameter
+        # user_id = self.request.user.id  # Current user id from the request body
+
+        client_object = get_object_or_404(Client, id=client_id)  # As one dictionary object, with exception
+
+        # client_object = Client.objects.filter(id=client_id).first()  # As one dictionary object, exception should be handled
+        # client_id_object = Client.objects.filter(id=client_id)  # As list with a dictionary element, exception should be handled
+        #
+        # if not client_object:
+        #     return Response(status=status.HTTP_404_NOT_FOUND,
+        #                     data={"message": gettext_lazy(NO_CLIENT_WITH_ID_MSG),
+        #                           "data": {}
+        #                           })
+
+        return client_object
+
+
+    def get(self, request, *args, **kwargs):
+        client = self.get_object()
+
+        serializer = self.serializer_class(instance=client,  # If one dictionary object, got with get_or_404()
+                                           context={"request": self.request})
+        # serializer=self.serializer_class(instance=client,
+        #                                  many=True,
+        #                                  context={"request":self.request})  # If list with one dictionary element, got with filter()
+
+        return Response(status=status.HTTP_200_OK,
+                        data={"message": gettext_lazy(CLIENT_DETAILS),
+                              "data": serializer.data})
+
+
+    def delete(self, request, *args, **kwargs):
+        user_is_superuser = request.user.is_superuser
+        if not user_is_superuser:
+            return Response(
+                status=status.HTTP_403_FORBIDDEN,
+                data={"message": gettext_lazy(NOT_SUPERUSER_FORBIDDEN)})
+
+        client = self.get_object()
+        client.delete()
+
+        return Response(status=status.HTTP_200_OK,
+                        data={"message": gettext_lazy(CLIENT_DELETED_MSG),
+                              "data": {}
+                              })
