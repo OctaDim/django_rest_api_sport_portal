9a68740f14a00503a65eda14a290067a40a98de9
Dima Mezhevich <octadim@gmail.com>
2024-03-17 15:07:49 +0300

    +url: Coach (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

--- apps/api/coach/urls.py
+++ apps/api/coach/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-# from apps.api.administrator.views import
+from apps.api.coach.views import (AllCoachesGenericList,
+                                   CreateNewCoachGenericCreate,
+                                   CoachByIdGenericRetrieveUpdate,
+                                   CoachByIdGenericRetrieveDestroy,
+                                   )
 
 app_name = "coaches"
 
 urlpatterns = [
+   path("all_coaches/",
+         AllCoachesGenericList.as_view(),
+         name="list_all-coaches"),
 
+    path("create_new_coach/",
+         CreateNewCoachGenericCreate.as_view(),
+         name="create-new-coach"),
+
+    path("coach_by_id_soft/<int:coach_id>/",
+         CoachByIdGenericRetrieveUpdate.as_view(),
+         name="coach-by-id-soft"),
+
+    path("coach_by_id_hard/<int:user_id>/",
+         CoachByIdGenericRetrieveDestroy.as_view(),
+         name="coach-by-id-hard"),
 ]
