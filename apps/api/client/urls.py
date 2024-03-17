d4f5b1a409afdf1843ca1301770b186cbad17091
Dima Mezhevich <octadim@gmail.com>
2024-03-17 15:01:38 +0300

    +url: Clients (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

--- apps/api/client/urls.py
+++ apps/api/client/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from apps.api.client.views import (AllClientsGenericList,
                                    CreateNewClientGenericCreate,
+                                   ClientByIdGenericRetrieveUpdate,
+                                   ClientByIdGenericRetrieveDestroy,
                                    )
 
 app_name = "clients"
@@ -12,13 +14,14 @@ urlpatterns = [
          name="list_all-clients"),
 
     path("create_new_client/",
-         CreateNewClientGenericCreate.as_view(), name="create-new-client"),
+         CreateNewClientGenericCreate.as_view(),
+         name="create-new-client"),
 
-    # path("client_by_id_soft/<int:client_id>/",
-    #      ClientByIdGenericRetrieveUpd.as_view(),
-    #      name="client-by-id-soft"),
-    #
-    # path("client_by_id_hard/<int:user_id>/",
-    #      ClientByIdGenericRetrieveUpdDestroy.as_view(),
-    #      name="client-by-id-hard"),
+    path("client_by_id_soft/<int:client_id>/",
+         ClientByIdGenericRetrieveUpdate.as_view(),
+         name="client-by-id-soft"),
+
+    path("client_by_id_hard/<int:client_id>/",
+         ClientByIdGenericRetrieveDestroy.as_view(),
+         name="client-by-id-hard"),
 ]
