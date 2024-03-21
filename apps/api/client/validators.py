84b5fe69fbf298f475aa0cadea24cfe11633ed40
Dima Mezhevich <octadim@gmail.com>
2024-03-21 15:17:29 +0300

    fix: Reading of the max limit image file was fixed by using utility converting function

--- apps/api/client/validators.py
+++ apps/api/client/validators.py
@@ -1,7 +1,10 @@
 from apps.api.client.settings import MAX_IMAGE_FILE_SIZE_IN_MB
-from apps.api.utils.utils import check_maximum_limit_image_file_size
+from apps.api.utils.utils import (check_maximum_limit_image_file_size,
+                                  number_or_str_to_abs_float,
+                                  )
 
 
 
 def validate_image_size(image):
-    check_maximum_limit_image_file_size(image, MAX_IMAGE_FILE_SIZE_IN_MB)
+    max_image_file_size = number_or_str_to_abs_float(MAX_IMAGE_FILE_SIZE_IN_MB)
+    check_maximum_limit_image_file_size(image, max_image_file_size)
