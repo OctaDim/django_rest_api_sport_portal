deb757f2c074aff92ffce36f99b8e46059d56bb2
Dima Mezhevich <octadim@gmail.com>
2024-03-22 23:46:16 +0300

    +url: Urls were added for group-client-progress (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

--- apps/api/group_client_progress/urls.py
+++ apps/api/group_client_progress/urls.py
@@ -1,28 +1,28 @@
 from django.urls import path
 
 from apps.api.group_client_progress.views import (
-                                    AllClientsProgressGenericList,
-                                    CreateNewClientProgressGenericCreate,
-                                    ClientProgressByIdGenericRetrieveUpdate,
-                                    ClientProgressByIdGenericRetrieveDestroy,
+    AllGroupsClientsProgressGenericList,
+    CreateNewGroupClientProgressGenericCreate,
+    GroupClientProgressByIdGenericRetrieveUpdate,
+    GroupClientProgressByIdGenericRetrieveDestroy,
                                     )
 
-app_name = "group-clients-progress"
+app_name = "groups-clients-progress"
 
 urlpatterns = [
     path("all_clients_progress/",
-          AllClientsProgressGenericList.as_view(),
-          name="list_all-clients-progress"),
+         AllGroupsClientsProgressGenericList.as_view(),
+         name="list_all-clients-progress"),
 
-    # path("create_new_client_progress/",
-    #      CreateNewClientProgressGenericCreate.as_view(),
-    #      name="create-new-client-progress"),
-    #
-    # path("client_progress_by_id_soft/<int:client_progress_id>/",
-    #      ClientProgressByIdGenericRetrieveUpdate.as_view(),
-    #      name="client-progress-by-id-soft"),
-    #
-    # path("client_progress_by_id_hard/<int:client_progress_id>/",
-    #      ClientProgressByIdGenericRetrieveDestroy.as_view(),
-    #      name="client-progress-by-id-hard"),
+    path("create_new_client_progress/",
+         CreateNewGroupClientProgressGenericCreate.as_view(),
+         name="create-new-client-progress"),
+
+    path("client_progress_by_id_soft/<int:client_progress_id>/",
+         GroupClientProgressByIdGenericRetrieveUpdate.as_view(),
+         name="client-progress-by-id-soft"),
+
+    path("client_progress_by_id_hard/<int:client_progress_id>/",
+         GroupClientProgressByIdGenericRetrieveDestroy.as_view(),
+         name="client-progress-by-id-hard"),
 ]
