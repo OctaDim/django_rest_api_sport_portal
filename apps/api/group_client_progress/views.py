690c6c9f61580c3b91162f76a4cd8b2811b3c1be
Dima Mezhevich <octadim@gmail.com>
2024-03-22 23:47:34 +0300

    +views: Views were added. GroupClientProgress (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

--- apps/api/group_client_progress/views.py
+++ apps/api/group_client_progress/views.py
@@ -33,30 +33,33 @@ from rest_framework.generics import (ListAPIView,
 
 from apps.api.group_client_progress.serializers import (
     GroupClientProgressAllFieldsModelSerializer,
-)
+    GroupClientProgressCreateModelSerializer,
+    GroupClientProgressRetrieveUpdateDeleteModelSerializer,
+                            )
 
 from apps.api.group_client_progress.models import GroupClientProgress
 from apps.api.user.models import User
 
 
 
-class AllClientsProgressGenericList(ListAPIView):
+class AllGroupsClientsProgressGenericList(ListAPIView):
     serializer_class = GroupClientProgressAllFieldsModelSerializer
 
     def get_queryset(self):
-        clients_progress = GroupClientProgress.objects.filter()
-        return clients_progress
+        groups_clients_progress = GroupClientProgress.objects.filter()
+        return groups_clients_progress
 
     def get(self, request: Request, *args, **kwargs):
-        clients_progress = self.get_queryset()
+        groups_clients_progress = self.get_queryset()
 
-        if not clients_progress:
+        if not groups_clients_progress:
             return Response(status=status.HTTP_204_NO_CONTENT,
                             data={"message": gettext_lazy(NO_CLIENTS_PROGRESS_MSG),
                                   "data": []}
                             )
 
-        serializer = self.serializer_class(instance=clients_progress, many=True)  # IMPORTANT: DON'T FORGET many=True
+        serializer = self.serializer_class(instance=groups_clients_progress,
+                                           many=True)  # IMPORTANT: DON'T FORGET many=True
         return Response(status=status.HTTP_200_OK,
                         data={"message": gettext_lazy(ALL_CLIENTS_PROGRESS_MSG),
                               "data": serializer.data}
@@ -64,15 +67,155 @@ class AllClientsProgressGenericList(ListAPIView):
 
 
 
-class CreateNewClientProgressGenericCreate(CreateAPIView):
-    pass
+class CreateNewGroupClientProgressGenericCreate(CreateAPIView):
+    serializer_class = GroupClientProgressCreateModelSerializer
+
+    def post(self, request: Request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={"request": self.request})
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK,
+                            data={"message": gettext_lazy(CLIENT_PROGRESS_CREATED_MSG),
+                                  "data": serializer.data})
+
+        return Response(status=status.HTTP_400_BAD_REQUEST,
+                        data={"message": gettext_lazy(CLIENT_PROGRESS_NOT_CREATED_MSG),
+                              "data": serializer.errors})
 
 
 
-class ClientProgressByIdGenericRetrieveUpdate(RetrieveUpdateAPIView):
-    pass
+class GroupClientProgressByIdGenericRetrieveUpdate(RetrieveUpdateAPIView):
+    serializer_class = GroupClientProgressRetrieveUpdateDeleteModelSerializer
+
+    def get_object(self, *args, **kwargs):
+        client_progress_id = self.kwargs.get("client_progress_id")  # Training_group id from the request additional parameter
+        # user_id = self.request.user.id  # Current user id from the request body
+
+        client_progress_object = get_object_or_404(GroupClientProgress,
+                                                  id=client_progress_id)  # As one dictionary object, with exception
+
+        # client_progress_object = GroupClientProgress.objects.filter(id=client_progress_id).first()  # As one dictionary object, exception should be handled
+        # client_progress_object = GroupClientProgress.objects.filter(id=client_progress_id)  # As list with a dictionary element, exception should be handled
+        #
+        # if not client_progress_object:
+        #     return Response(status=status.HTTP_404_NOT_FOUND,
+        #                     data={"message": gettext_lazy(NO_CLIENT_PROGRESS_WITH_ID_MSG),
+        #                           "data": {}
+        #                           })
+
+        return client_progress_object
+
+    def get(self, request, *args, **kwargs):
+        client_progress = self.get_object()
+
+        serializer = self.serializer_class(instance=client_progress,  # If one dictionary object, got with get_or_404()
+                                           context={"request": self.request})
+        # serializer=self.serializer_class(instance=client_progress,
+        #                                  many=True,
+        #                                  context={"request":self.request})  # If list with one dictionary element, got with filter()
+
+        return Response(status=status.HTTP_200_OK,
+                        data={"message": gettext_lazy(CLIENT_PROGRESS_DETAILS),
+                              "data": serializer.data})
+
+    def patch(self, request, *args, **kwargs):  # IMPORTANT: Only if PATCH, entered values stay after validation
+        client_progress = self.get_object()
+
+        client_progress_id = self.kwargs.get("client_progress_id")
+        serializer = self.serializer_class(
+                            instance=client_progress,
+                            data=request.data,
+                            partial=True,
+                            context={"client_progress_id": client_progress_id,
+                                     # Additionally transferred for serializer validation
+                                     "request": self.request,
+                                     }
+                            )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(status=status.HTTP_200_OK,
+                            data={"message": gettext_lazy(CLIENT_PROGRESS_UPDATED_MSG),
+                                  "data": serializer.data})
+
+        return Response(status=status.HTTP_400_BAD_REQUEST,
+                        data={"message": gettext_lazy(CLIENT_PROGRESS_NOT_UPDATED_MSG),
+                              "data": serializer.errors})
+
+    def put(self, request, *args, **kwargs):  # IMPORTANT: Only if PATCH, entered values stay after validation
+        client_progress = self.get_object()
+
+        client_progress_id = self.kwargs.get("client_progress_id")
+        serializer = self.serializer_class(
+                            instance=client_progress,
+                            data=request.data,
+                            partial=True,
+                            context={"client_progress_id": client_progress_id,
+                                     # Additionally transferred for serializer validation
+                                     "request": self.request,
+                                     }
+                            )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(status=status.HTTP_200_OK,
+                            data={"message": gettext_lazy(CLIENT_PROGRESS_UPDATED_MSG),
+                                  "data": serializer.data})
+
+        return Response(status=status.HTTP_400_BAD_REQUEST,
+                        data={"message": gettext_lazy(CLIENT_PROGRESS_NOT_UPDATED_MSG),
+                              "data": serializer.errors})
 
 
+class GroupClientProgressByIdGenericRetrieveDestroy(RetrieveDestroyAPIView):
+    serializer_class = GroupClientProgressRetrieveUpdateDeleteModelSerializer
 
-class ClientProgressByIdGenericRetrieveDestroy(RetrieveDestroyAPIView):
-    pass
+    def get_object(self):
+        client_progress_id = self.kwargs.get("client_progress_id")  # Training_group id from the request additional parameter
+        # user_id = self.request.user.id  # Current user id from the request body
+
+        client_progress_object = get_object_or_404(GroupClientProgress,
+                                                   id=client_progress_id)  # As one dictionary object, with exception
+
+        # client_progress_object = GroupClientProgress.objects.filter(id=client_progress_id).first()  # As one dictionary object, exception should be handled
+        # client_progress_object = GroupClientProgress.objects.filter(id=client_progress_id)  # As list with a dictionary element, exception should be handled
+        #
+        # if not client_progress_object:
+        #     return Response(status=status.HTTP_404_NOT_FOUND,
+        #                     data={"message": gettext_lazy(NO_CLIENT_PROGRESS_WITH_ID_MSG),
+        #                           "data": {}
+        #                           })
+
+        return client_progress_object
+
+    def get(self, request, *args, **kwargs):
+        client_progress = self.get_object()
+
+        serializer = self.serializer_class(instance=client_progress,  # If one dictionary object, got with get_or_404()
+                                           context={"request": self.request})
+        # serializer=self.serializer_class(instance=client_progress,
+        #                                  many=True,
+        #                                  context={"request":self.request})  # If list with one dictionary element, got with filter()
+
+        return Response(status=status.HTTP_200_OK,
+                        data={"message": gettext_lazy(CLIENT_PROGRESS_DETAILS),
+                              "data": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        user_is_superuser = request.user.is_superuser
+        if not user_is_superuser:
+            return Response(
+                status=status.HTTP_403_FORBIDDEN,
+                data={"message": gettext_lazy(NOT_SUPERUSER_HARD_DELETE_FORBIDDEN)})
+
+        client_progress = self.get_object()
+        client_progress.delete()
+
+        return Response(status=status.HTTP_200_OK,
+                        data={"message": gettext_lazy(CLIENT_PROGRESS_DELETED_MSG),
+                              "data": {}
+                              })
