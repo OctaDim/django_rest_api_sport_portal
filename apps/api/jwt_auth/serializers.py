04ccd50a3067f0a742bc32d68d2e38d5c6607b13
Dima Mezhevich <octadim@gmail.com>
2024-03-23 22:07:32 +0300

    +serializer: Custom JWT token serializer was added with all additional user properties in the token

---
+++ apps/api/jwt_auth/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework_simplejwt.serializers import (TokenObtainPairSerializer,
+                                                  AuthUser,
+                                                  )
+
+from rest_framework_simplejwt.tokens import Token
+
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user: AuthUser) -> Token:
+        token = super().get_token(user=user)
+
+        token["email"] = user.email
+        token["username"] = user.username
+        token["nickname"] = user.nickname
+        token["is_active"] = user.is_active
+        token["is_staff"] = user.is_staff
+        token["is_superuser"] = user.is_superuser
+        token["is_trainer"] = user.is_trainer
+
+        return token
