f8675baec6c26ae2cc4b7ed94eba785ba47ffd15
Dima Mezhevich <octadim@gmail.com>
2024-03-25 16:47:43 +0300

    minor renaming app setting variables

--- apps/api/self_satisfaction_level/settings.py
+++ apps/api/self_satisfaction_level/settings.py
@@ -1,7 +1,7 @@
 # Self satisfaction level minimum level value:
 # integer or string with integer (if float -> will be truncated to integer)
-SATISFACTION_LEVEL_MIN_LIMIT = -50
+SATISFACTION_LEVEL_VALUE_MIN_LIMIT = -10
 
 # Self satisfaction level maximum level value:
 # integer or string with integer (if float -> will be truncated to integer)
-SATISFACTION_LEVEL_MAX_LIMIT = 50
+SATISFACTION_LEVEL_VALUE_MAX_LIMIT = 10
--- apps/api/self_satisfaction_level/validators.py
+++ apps/api/self_satisfaction_level/validators.py
@@ -1,11 +1,11 @@
 from apps.api.utils.utils import number_or_str_to_int
 from apps.api.utils.utils import validate_level_value
 from apps.api.self_satisfaction_level.settings import (
-                                        SATISFACTION_LEVEL_MIN_LIMIT,
-                                        SATISFACTION_LEVEL_MAX_LIMIT)
+    SATISFACTION_LEVEL_VALUE_MIN_LIMIT,
+    SATISFACTION_LEVEL_VALUE_MAX_LIMIT)
 
 
 def validate_satisfaction_level_value(value):
-    level_min_limit = number_or_str_to_int(SATISFACTION_LEVEL_MIN_LIMIT)
-    level_max_limit = number_or_str_to_int(SATISFACTION_LEVEL_MAX_LIMIT)
+    level_min_limit = number_or_str_to_int(SATISFACTION_LEVEL_VALUE_MIN_LIMIT)
+    level_max_limit = number_or_str_to_int(SATISFACTION_LEVEL_VALUE_MAX_LIMIT)
     validate_level_value(value, level_min_limit, level_max_limit)
