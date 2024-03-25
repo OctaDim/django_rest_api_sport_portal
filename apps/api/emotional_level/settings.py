f11cd3dd369cfbceeae91f85b19d02bc8aa0be7d
Dima Mezhevich <octadim@gmail.com>
2024-03-25 15:52:17 +0300

    fix: minor renaming setting variables

--- apps/api/emotional_level/settings.py
+++ apps/api/emotional_level/settings.py
@@ -1,7 +1,7 @@
 # Emotional level minimum level value:
 # integer or string with integer (if float -> will be truncated to integer)
-EMOTIONAL_LEVEL_MIN_LIMIT = -10
+EMOTIONAL_LEVEL_VALUE_MIN_LIMIT = -10
 
 # Emotional level maximum level value:
 # integer or string with integer (if float -> will be truncated to integer)
-EMOTIONAL_LEVEL_MAX_LIMIT = 10
+EMOTIONAL_LEVEL_VALUE_MAX_LIMIT = 10
