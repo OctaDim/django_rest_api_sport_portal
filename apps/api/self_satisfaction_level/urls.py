d6d5d07f6d4b18608b49e1155fec9eaa99f83815
Dima Mezhevich <octadim@gmail.com>
2024-03-25 22:44:42 +0300

    +url: Urls were added for self_satisfaction_level (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

---
+++ apps/api/self_satisfaction_level/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+
+from apps.api.self_satisfaction_level.views import (
+                            AllSatisfactionLevelsGenericList,
+                            CreateNewSatisfactionLevelGenericCreate,
+                            SatisfactionLevelByIdGenericRetrieveUpdate,
+                            SatisfactionLevelByIdGenericRetrieveDestroy,
+                            )
+
+app_name = "satisfaction_level"
+
+urlpatterns = [
+   path("all_satisfaction_levels/",
+         AllSatisfactionLevelsGenericList.as_view(),
+         name="list_all-satisfaction-levels"),
+
+    path("create_new_satisfaction_level/",
+         CreateNewSatisfactionLevelGenericCreate.as_view(),
+         name="create-new-satisfaction-level"),
+
+    path("satisfaction_level_by_id_soft/<int:satisfaction_level_id>/",
+         SatisfactionLevelByIdGenericRetrieveUpdate.as_view(),
+         name="satisfaction-level-by-id-soft"),
+
+    path("satisfaction_level_by_id_hard/<int:satisfaction_level_id>/",
+         SatisfactionLevelByIdGenericRetrieveDestroy.as_view(),
+         name="satisfaction-level-by-id-hard"),
+
+]
