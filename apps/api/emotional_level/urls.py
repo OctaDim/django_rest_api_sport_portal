7423b98c3988457c7d0c341193de30466a51c01d
Dima Mezhevich <octadim@gmail.com>
2024-03-25 15:47:37 +0300

    +url: Urls were added for emotional_level (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

---
+++ apps/api/emotional_level/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+
+from apps.api.emotional_level.views import (
+                                  AllEmotionalLevelsGenericList,
+                                  CreateNewEmotionalLevelGenericCreate,
+                                  EmotionalLevelByIdGenericRetrieveUpdate,
+                                  EmotionalLevelByIdGenericRetrieveDestroy,
+                                  )
+
+app_name = "emotional_level"
+
+urlpatterns = [
+   path("all_emotional_levels/",
+         AllEmotionalLevelsGenericList.as_view(),
+         name="list_all-emotional-levels"),
+
+    path("create_new_emotional_level/",
+         CreateNewEmotionalLevelGenericCreate.as_view(),
+         name="create-new-emotional-level"),
+
+    path("emotional_level_by_id_soft/<int:emotional_level_id>/",
+         EmotionalLevelByIdGenericRetrieveUpdate.as_view(),
+         name="emotional_level-by-id-soft"),
+
+    path("emotional_level_by_id_hard/<int:emotional_level_id>/",
+         EmotionalLevelByIdGenericRetrieveDestroy.as_view(),
+         name="emotional_level-by-id-hard"),
+
+]
