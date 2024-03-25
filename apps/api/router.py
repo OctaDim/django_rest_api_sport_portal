9222bf17e5afc8ad67df1f56f19a0a30c2d9e053
Dima Mezhevich <octadim@gmail.com>
2024-03-25 22:43:31 +0300

    +router: Url satisfaction_levels/ was added

--- apps/api/router.py
+++ apps/api/router.py
@@ -7,13 +7,17 @@ urlpatterns = [
     path('administrators/', include("apps.api.administrator.urls")),
     path('clients/', include("apps.api.client.urls")),
     path('coaches/', include("apps.api.coach.urls")),
+
     path('departments/', include("apps.api.department.urls")),
     path('training_groups/', include("apps.api.training_group.urls")),
     path('clients_progress/', include("apps.api.group_client_progress.urls")),
+
     path('authentication_jwt/', include("apps.api.authentication_jwt.urls")),
     path('authentication/', include("apps.api.authentication.urls")),
 
     path('emotional_levels/', include("apps.api.emotional_level.urls")),
+    path('satisfaction_levels/', include("apps.api.self_satisfaction_level.urls")),
+
     # path('client_statuses/', include("apps.api.client_status.urls")),
 
 ]
