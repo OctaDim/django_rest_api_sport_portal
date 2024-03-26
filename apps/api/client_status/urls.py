62959ef9af8c44ed54cb91e9b2411bb928b487a8
Dima Mezhevich <octadim@gmail.com>
2024-03-26 03:13:41 +0300

    +url: Urls were added for client_statuses (all, create, retrieve by id, update + soft del, retrieve by id + hard del)

---
+++ apps/api/client_status/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+
+from apps.api.client_status.views import (
+                                  AllClientStatusesGenericList,
+                                  CreateNewClientStatusGenericCreate,
+                                  ClientStatusByIdGenericRetrieveUpdate,
+                                  ClientStatusByIdGenericRetrieveDestroy,
+                                  )
+
+app_name = "client_status"
+
+urlpatterns = [
+   path("all_client_statuses/",
+         AllClientStatusesGenericList.as_view(),
+         name="list_all-client-statuses"),
+
+    path("create_new_client_status/",
+         CreateNewClientStatusGenericCreate.as_view(),
+         name="create-new-client-status"),
+
+    path("client_status_by_id_soft/<int:client_status_id>/",
+         ClientStatusByIdGenericRetrieveUpdate.as_view(),
+         name="client_status-by-id-soft"),
+
+    path("client_status_by_id_hard/<int:client_status_id>/",
+         ClientStatusByIdGenericRetrieveDestroy.as_view(),
+         name="client_status-by-id-hard"),
+
+]
