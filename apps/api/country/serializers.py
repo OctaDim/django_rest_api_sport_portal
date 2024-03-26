42e0503f3af418b36495d458bfea0b5aaef5485f
Dima Mezhevich <octadim@gmail.com>
2024-03-26 11:29:04 +0300

    +serializer: Serializers for 'country' app were added  with all additional and overridden methods

---
+++ apps/api/country/serializers.py
@@ -0,0 +1,173 @@
+from rest_framework import serializers, request
+
+from django.utils.translation import gettext_lazy
+
+from apps.api.messages_api.messages_non_front import EXCEPTION_INFO
+
+from apps.api.messages_api.messages_errors import (CREATOR_REQUIRED,
+                                                   COUNTRY_REQUIRED,
+                                                   COUNTRY_EXISTS)
+
+from apps.api.country.models import Country
+from apps.api.user.models import User
+
+
+
+class CountryAllFieldsModelSerializer(serializers.ModelSerializer):
+    creator = serializers.SlugRelatedField(slug_field="full_name",
+                                           read_only=True)
+
+    class Meta:
+        model = Country
+        # fields = "__all__"
+        fields = ["id",
+                  "name",
+                  "description",
+                  "created_at",
+                  "updated_at",
+                  "creator",
+                  ]
+
+class CountryCreateModelSerializer(serializers.ModelSerializer):
+    creator = serializers.SlugRelatedField(slug_field="id",
+                                           read_only=False,
+                                           queryset=User.objects.all())
+
+    class Meta:
+        model = Country
+        # fields = "__all__"
+        fields = ["id",
+                  "name",
+                  "description",
+                  "created_at",
+                  "updated_at",
+                  "creator",
+                  ]
+
+    def get_fields(self):
+        fields = super().get_fields()
+
+        try:
+            current_user_id = self.context["request"].user.id
+            fields["creator"].queryset = User.objects.filter(id=current_user_id)
+            return fields
+        except (ValueError, Exception) as error:
+            print(gettext_lazy(EXCEPTION_INFO(error)))
+        return fields
+
+
+    def to_representation(self, instance):  # Forms dictionary with response fields (fields-keys can be added)
+        representation = super().to_representation(instance)
+        representation["creator"] = str(instance.creator)
+        representation["creator_id"] = instance.creator.id
+        return representation
+
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+
+        error_messages = []
+
+        name_in_attr = attrs.get("name")
+        creator_in_attr = attrs.get("creator")
+
+        if not name_in_attr:
+            error_messages.append(COUNTRY_REQUIRED)
+            # raise serializers.ValidationError(
+            #     gettext_lazy(COUNTRY_REQUIRED))
+
+        if not creator_in_attr:
+            error_messages.append(CREATOR_REQUIRED)
+            # raise serializers.ValidationError(
+            #     gettext_lazy(CREATOR_REQUIRED))
+
+        if Country.objects.filter(name=name_in_attr).exists():
+            error_messages.append(COUNTRY_EXISTS)
+            # raise serializers.ValidationError(
+            #     gettext_lazy(COUNTRY_EXISTS))
+
+        if error_messages:
+            error_messages_str = ", ".join(error_messages)
+            raise serializers.ValidationError(
+                gettext_lazy(error_messages_str))
+
+        return attrs
+
+
+
+class CountryRetrieveUpdateDeleteModelSerializer(serializers.ModelSerializer):
+    creator = serializers.SlugRelatedField(slug_field="id",
+                                           read_only=False,
+                                           queryset=User.objects.all())
+
+    class Meta:
+        model = Country
+        # fields = "__all__"
+        fields = ["id",
+                  "name",
+                  "description",
+                  "created_at",
+                  "updated_at",
+                  "creator",
+                  ]
+
+    def get_fields(self):
+        fields = super().get_fields()
+
+        try:
+            current_user_id = self.context["request"].user.id
+            fields["creator"].queryset = User.objects.filter(id=current_user_id)
+            return fields
+        except (ValueError, Exception) as error:
+            print(gettext_lazy(EXCEPTION_INFO(error)))
+        return fields
+
+
+    def to_representation(self, instance):  # Forms dictionary with response fields (fields-keys can be added)
+        representation = super().to_representation(instance)
+        representation["creator"] = str(instance.creator)
+        representation["creator_id"] = instance.creator.id
+        return representation
+
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+
+        error_messages = []
+
+        name_in_attr = attrs.get("name")
+        creator_in_attr = attrs.get("creator")
+
+        if not name_in_attr:
+            error_messages.append(COUNTRY_REQUIRED)
+            # raise serializers.ValidationError(
+            #     gettext_lazy(COUNTRY_REQUIRED))
+
+        if not creator_in_attr:
+            error_messages.append(CREATOR_REQUIRED)
+            # raise serializers.ValidationError(
+            #     gettext_lazy(CREATOR_REQUIRED))
+
+
+        country_id_view_passed_req_param = (
+            self.context.get("country_id"))
+
+        old_country_name = Country.objects.get(
+            id=country_id_view_passed_req_param).name
+
+        new_country_name = attrs.get("name")
+
+        if new_country_name != old_country_name:
+            if Country.objects.filter(
+                    name=new_country_name).exists():
+                error_messages.append(COUNTRY_EXISTS)
+                # raise serializers.ValidationError(
+                #     gettext_lazy(COUNTRY_EXISTS))
+
+
+        if error_messages:
+            error_messages_str = ", ".join(error_messages)
+            raise serializers.ValidationError(
+                gettext_lazy(error_messages_str))
+
+        return attrs
