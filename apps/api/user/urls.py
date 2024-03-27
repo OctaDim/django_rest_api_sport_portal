01dfe962fdb26e6ad32d97c94c55f4d6689afa8b
Dima Mezhevich <octadim@gmail.com>
2024-03-27 03:27:12 +0300

    fix: Urls references and urls names in 'user' were corrected

--- apps/api/user/urls.py
+++ apps/api/user/urls.py
@@ -19,20 +19,20 @@ urlpatterns = [
     # superuser
     path("register_new_superuser/", RegisterNewSuperUserGenericCreate.as_view(), name="register-new-superuser"),
     path("list_all_users_all_fields/", AllUsersGenericListForSuperuser.as_view(), name="list-all-users-all-fields"),
-    path("user_info_by_id_hard_del/<int:user_id>/", UserInfoByIdGenericRetrieveUpdDestroy.as_view(), name="user-info-by-id-hard-del"),
+    path("user_by_id_hard/<int:user_id>/", UserInfoByIdGenericRetrieveUpdDestroy.as_view(), name="user-by-id-hard"),
 
     # staff, superuser
     path("register_new_user/", RegisterNewUserGenericCreate.as_view(), name="register-new-user"),
     path("register_new_staff_user/", RegisterNewStaffUserGenericCreate.as_view(), name="register-new-staff-user"),
     path("register_new_trainer_user/", RegisterNewTrainerUserGenericCreate.as_view(), name="register-new-trainer-user"),
     path("list_all_users_limit_fields/", AllUsersGenericListForStaff.as_view(), name="list-all-users-limit-fields"),
-    path("user_info_by_id_soft_del/<int:user_id>/", UserInfoByIdGenericRetrieveUpdate.as_view(), name="user-info-by-id-soft-del"),
+    path("user_by_id_soft/<int:user_id>/", UserInfoByIdGenericRetrieveUpdate.as_view(), name="user-by-id-soft"),
 
     # ################### MY TRIAL CODE ################################
-    path("_test_register_new_user_v2/", _Test_RegisterNewUserGenericCreate.as_view(), name="test-register-new-user-v2"),
-    path("_test_register_new_staff_user_v2/", _Test_RegisterNewStaffUserGenericCreate.as_view(), name="test-register-new-staff-user-2"),
-    path("_test_register_new_trainer_user_v2/", _Test_RegisterNewTrainerUserGenericCreate.as_view(), name="test-register-new-trainer-user-v2"),
-    path("_test_register_new_superuser_v2/", _Test_RegisterNewSuperUserGenericCreate.as_view(), name="test-register-new-superuser-v2"),
+    path("_test_register_new_user/", _Test_RegisterNewUserGenericCreate.as_view(), name="test-register-new-user"),
+    path("_test_register_new_staff_user/", _Test_RegisterNewStaffUserGenericCreate.as_view(), name="test-register-new-staff-user"),
+    path("_test_register_new_trainer/", _Test_RegisterNewTrainerUserGenericCreate.as_view(), name="test-register-new-trainer-user"),
+    path("_test_register_new_superuser/", _Test_RegisterNewSuperUserGenericCreate.as_view(), name="test-register-new-superuser"),
     # ##################################################################
 
 ]
